2dd4d37c61ae955cf3c196a91ca394264790b353
mikailaAKeredolu <makerofappsdotcom@gmail.com>
2024-12-05 13:00:58 -0500

    added an SLF4J logger with a demo

--- src/main/java/com/zipcoder/Demotwo_c16/controller/JokeController.java
+++ src/main/java/com/zipcoder/Demotwo_c16/controller/JokeController.java
@@ -2,21 +2,23 @@ package com.zipcoder.Demotwo_c16.controller;
 
 import com.zipcoder.Demotwo_c16.model.Joke;
 import com.zipcoder.Demotwo_c16.service.JokeService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class JokeController {
 
+    private  final Logger logger = LoggerFactory.getLogger(JokeController.class);
+
     @Autowired
     JokeService jokeService;
-
-    //define my route
-
 
     @PostMapping("/jokes")
     public void createJoke(@RequestBody Joke joke){
         jokeService.addJoke(joke);
+        logger.info("Created a joke successfully");
     }
 
     @GetMapping("/jokes")
@@ -32,6 +34,7 @@ public class JokeController {
 
     @DeleteMapping("/jokes/{id}")
     public void deleteJoke(@PathVariable Long id){
+        logger.info("deleted a joke with id of " + id);
         jokeService.deleteJokeById(id);
     }
 
